6768766066c3264766a618e2314ce3293529cf79
Tahir77764 <tahirbaba907199@gmail.com>
2025-09-26 20:01:09 +0530

    updating java file

--- Sum.java
+++ Sum.java
@@ -2,7 +2,6 @@ public class Sum {
   public static void main(String[] args) {
     int a = 2;
     int b = 9;
-    int sum = a + b;
-    System.out.println(sum);
+    System.out.println(a + b);
   }
 }
